3f8015e2a2ccec82aa466db388b2ceb7a46ae8d6
Darshan <darshan@livr.co>
2025-11-05 11:15:33 +0000

    Exporting it through scene.gltf

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ async function initAR() {
 
     const loader = new GLTFLoader();
     loader.load(
-        './scene.glb',
+        './scene.gltf',
         (gltf) => {
             console.log("[WebAR] GLB loaded");
             gltfRoot = gltf.scene;
