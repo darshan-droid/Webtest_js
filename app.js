c041fcf37f69f5404350f0f67f58c05ae3bf56f0
Antonio <antonio@livr.co>
2025-11-10 13:58:32 +0000

    Update app.js

--- app.js
+++ app.js
@@ -244,8 +244,8 @@ function sendThreejsInput(button) {
 //global declaration for unity instance
 //const { createUnityInstance } = await import("./Build/UnityProject.loader.js");
 async function UnityLoader() {
-    const unityCanvas = document.getElementById('unity-canvas');
-    unityCanvas.style.display = 'block'; // make visible if needed
+    //const unityCanvas = document.getElementById('unity-canvas');
+    //unityCanvas.style.display = 'block'; // make visible if needed
 
     const buildUrl = "./Build"; // folder with Unity files
     const config = {
